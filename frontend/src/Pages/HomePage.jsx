6b1fb6209d9a21f97a633a1a2a36811fba412723
Jaroslav Zaba <jaroslav.zaba@post.cz>
2024-11-27 18:37:16 +0100

    BUGFIX: idle logout works

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Stack, Box, Title, Text, Group, Button } from '@mantine/core'
-import { useIdle } from '@mantine/hooks'
+import { useInterval } from '@mantine/hooks'
 import Cookies from 'js-cookie'
 
 import SubHomePageAdmin from './SubHomePage/SubHomePageAdmin.jsx'
@@ -12,7 +12,29 @@ import axios from 'axios'
 
 function HomePage() {
   const [content, setContent] = useState(<></>)
-  const idle = useIdle(600000, { initialState: false })
+  const [logoutCounter, setLogoutCounter] = useState(0)
+  const i = useInterval(() => setLogoutCounter((prev) => prev+1), 5000)
+
+  useEffect(() => {
+    i.start()
+    return i.stop
+  }, [])
+
+  useEffect(() => {
+    const events = ['click', 'mousemove', 'keypress', 'scroll']
+
+    const update = () => {setLogoutCounter(0)}
+
+    events.forEach((event) => window.addEventListener(event, update))
+
+    return () => {events.forEach((event) => window.removeEventListener(event, update))}
+  })
+
+  useEffect(() => {
+    if (logoutCounter > 120) {
+      logout()
+    }
+  }, [logoutCounter])
 
   if (Cookies.get('JWT_token') === undefined) {
     window.location.href = '/'
@@ -65,7 +87,6 @@ function HomePage() {
         </Group>
       </Box>
     </Stack>
-    {idle ? logout() : <></>}
     </>
   )
 }
