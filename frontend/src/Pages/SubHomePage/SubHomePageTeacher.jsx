7a4d264fab1b1876d39583388a9568e93dbcbe9d
Jaroslav Zaba <jaroslav.zaba@post.cz>
2024-12-17 17:05:44 +0100

    Default display schedule

--- frontend/src/Pages/SubHomePage/SubHomePageTeacher.jsx
+++ frontend/src/Pages/SubHomePage/SubHomePageTeacher.jsx
@@ -1,9 +1,27 @@
+import { Group, Menu, Text, Button } from '@mantine/core';
+import { useEffect } from 'react';
+import Cookies from 'js-cookie'
+
+
+import Schedule from '../../Components/Schedule';
+
 
 function SubHomePageTeacher({setContent}) {
+
+  useEffect(() => {
+    if (Cookies.get('JWT_token') === undefined) {
+        window.location.href = '/'
+      }
+    
+    let token = JSON.parse(atob(Cookies.get('JWT_token').split('.')[0]))
+    setContent(<Schedule aFieldType='T' aIdToFetch={token['uid']} />)
+  }, [])
   
   return (
     <>
-    TEACHER PORTAL
+      <Group align='center' h={"100%"}>
+          
+      </Group>
     </>
   );
 }
